de668e8e1d935b500331689d2c6e308baa2f557d
beargame123 <hoyoung7827@gmail.com>
2022-04-14 23:41:30 +0900

    ➕ Test코드 개발

--- Tsinghua-University/src/test/java/com/example/tsinghuauniversity/CommentTest.java
+++ Tsinghua-University/src/test/java/com/example/tsinghuauniversity/CommentTest.java
@@ -1,4 +1,44 @@
 package com.example.tsinghuauniversity;
 
+import com.example.tsinghuauniversity.comment.entity.Comment;
+import com.example.tsinghuauniversity.comment.repository.CommentRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class CommentTest {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public Comment create(){
+        Comment comment = Comment.builder()
+                .comment("동의합니다")
+                .build();
+        return comment;
+    }
+
+    @Test
+    public void saveTest(){
+        var comment = create();
+        var expected = commentRepository.save(comment);
+
+        Assertions.assertNotNull(expected);
+        Assertions.assertEquals(1, expected.getId());
+    }
+
+    @Test
+    public void commentList(){
+        Comment comment1 = create();
+        commentRepository.save(comment1);
+
+        Comment comment2 = create();
+        commentRepository.save(comment2);
+
+        int count = commentRepository.findAll().size();
+        Assertions.assertEquals(2, count);
+
+    }
 }
--- Tsinghua-University/src/test/java/com/example/tsinghuauniversity/PostTest.java
+++ Tsinghua-University/src/test/java/com/example/tsinghuauniversity/PostTest.java
@@ -1,10 +1,49 @@
 package com.example.tsinghuauniversity;
 
+import com.example.tsinghuauniversity.post.entity.Post;
+import com.example.tsinghuauniversity.post.repository.PostRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+
+@SpringBootTest
 public class PostTest {
-    @Test
-    void Test(){
 
+    @Autowired
+    private PostRepository postRepository;
+
+
+    public Post test(){
+        var post = Post.builder()
+                .title("저녁 점호를 없애주세요")
+                .content("제곧내")
+                .category("기숙사")
+                .build();
+        return post;
     }
+
+    @Test
+    public void saveTest(){
+        var post = test();
+        var expected = postRepository.save(post);
+
+        Assertions.assertNotNull(expected);
+        Assertions.assertEquals(1, expected.getId());
+    }
+
+    @Test
+    public void listAllTest(){
+        var post1 = test();
+        postRepository.save(post1);
+
+        var post2 = test();
+        postRepository.save(post2);
+
+        int count = postRepository.findAll().size();
+
+        Assertions.assertEquals(2, count);
+    }
+
 }
